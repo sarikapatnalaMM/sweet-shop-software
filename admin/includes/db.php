d37c2f914a76bc5518c1c9f5619462559928d5b2
sarikapatnala <sarikapatnala2@gmail.com>
2024-07-09 16:49:16 +0530

    ghughegherrge

--- admin/includes/db.php
+++ admin/includes/db.php
@@ -6,6 +6,9 @@ $pass = '';
  
 
 
+
+
+
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -13,5 +16,3 @@ try {
     die("Could not connect to the database $db :" . $e->getMessage());
 }
 ?>
-
-
