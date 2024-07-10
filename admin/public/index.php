d3a7aec8b3d77352c1e468a98e38a5543f6343ea
Ramesh92340 <rameshpilli1428@gmail.com>
2024-07-10 12:44:17 +0530

    files added

--- admin/public/index.php
+++ admin/public/index.php
@@ -21,10 +21,10 @@
 
 </head>
 
-<body id="page-top" >
+<body id="page-top">
 
     <!-- Page Wrapper -->
-    <div id="wrapper" >
+    <div id="wrapper">
 
         <!-- Sidebar -->
         <?php
@@ -46,25 +46,9 @@
 
                 <!-- Begin Page Content -->
                 <div class="container-fluid ">
-
                     <!-- Page Heading -->
-                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-                         
-                    </div>
-
-
-                    <!-- Content Row -->
-                    <style>
-                        .card-custom {
-                            margin: 6px;
-                            /* Reset margin to prevent extra space */
-                        }
-                    </style>
-                    </head>
-
                     <body>
-                        <div class="container">
+                        <!-- <div class="container">
                             <div class="row">
                                 <div class="d-sm-flex align-items-center justify-content-between mb-4">
                                     <h2 class="h2 mb-0 text-info mx-2">Recently Published Blogs</h2>
@@ -72,19 +56,15 @@
                                 <div class='row row-custom no-gutters'>
 
                                     <?php
-                                    // Database connection (replace with your actual database connection details)
                                     include '../../db.connection/db_connection.php';
 
-
-                                    // Fetch blog data
+ 
                                     $sql = "SELECT id, title, content, video FROM blog";
                                     $result = $conn->query($sql);
 
                                     if ($result->num_rows > 0) {
                                         while ($row = $result->fetch_assoc()) {
-                                            // $photos = json_decode($row['photos'], true);
-                                            // $first_photo = isset($photos[0]) ? $photos[0] : "https://mailrelay.com/wp-content/uploads/2018/03/que-es-un-blog-1.png";
-                                            echo "
+                                             echo "
                                             <div class='col-12 col-md-4 col-custom'>
                                             <div class='card card-custom'>
                                             <video src='./uploads/videos/{$row['video']}' class='card-img-top' autoplay muted controls>
@@ -111,11 +91,105 @@
                                     $conn->close();
                                     ?>
                                 </div>
-                            </div> <!-- Pie Chart -->
+                            </div>                           
+                        </div> -->
 
+                        <div class="container">
+                        <div class="row justify-content-center">
+                <div class="col-md-12 staffBox">
+                    <div class="col-md-4 staff1">
+                        <ul class="staffui">
+                            <h6 class="staffhead">+ Add Staff</h6>
+                            <li class="StaffLi">Detailes</li>
+                            <li class="StaffLi">Salaries</li>
+                            <li class="StaffLi">Incharges</li>
+                            <li class="StaffLi">Salaries</li>
+                        </ul>
+                    </div>
+                    <div class="col-md-8 staff2">
+
+                        <form method="post" enctype="multipart/form-data">
+                            <div class="form-group">
+                                <div class="row">
+                                    <div class="formStaffBox">
+                                        <div class="col-md-6">
+                                            <h6 class="AddStaff">Add Staff People</h6>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <h6 class="KakinadaBranch">Kakinada Branch</h6><svg class="kkdIcon" xmlns="http://www.w3.org/2000/svg" width="20" height="19" viewBox="0 0 20 19" fill="none">
+                                                <path d="M17.7178 4.96555L10 12.1861L2.28216 4.96555C1.67358 4.33767 1.06501 4.32459 0.456432 4.9263C-0.152144 5.52802 -0.152144 6.11665 0.456432 6.6922L9.08714 14.8546C9.30844 15.1162 9.61272 15.247 10 15.247C10.3873 15.247 10.6916 15.1162 10.9129 14.8546L19.5436 6.6922C20.1521 6.11665 20.1521 5.52802 19.5436 4.9263C18.935 4.32459 18.3264 4.33767 17.7178 4.96555Z" fill="#202224" />
+                                            </svg>
+                                        </div>
+                                    </div>
+                                    <div class="staffFrm">
+                                        <div class="col-md-6 inputbox">
+                                            <label class="control-label mb-2 inputIn">Upload Photo:</label>
+                                            <input type="file" class="form-control inputInn" name="buildermail">
+                                        </div>
+                                        <div class="form-group col-md-6 inputbox">
+                                            <label class="control-label mb-2 inputIn">Person Name :</label>
+                                            <input type="text" class="form-control inputInn" name="buildermail">
+                                        </div>
+                                    </div>
+                                    <div class="staffFrm">
+                                        <div class="col-md-6 inputbox">
+                                            <label class="control-label mb-2 inputIn">Address:</label>
+                                            <input type="text" class="form-control inputInn" name="buildermail">
+                                        </div>
+                                        <div class="form-group col-md-6 inputbox">
+                                            <label class="control-label mb-2 inputIn">Age :</label>
+                                            <input type="number" class="form-control inputInn" name="buildermail">
+                                        </div>
+                                    </div>
+                                    <div class="staffFrm">
+                                        <div class="col-md-6 inputbox">
+                                            <label class="control-label mb-2 inputIn">Account Number:</label>
+                                            <input type="number" class="form-control inputInn" name="buildermail">
+                                        </div>
+                                        <div class="form-group col-md-6 inputbox">
+                                            <label class="control-label mb-2 inputIn">Phone Number :</label>
+                                            <input type="number" class="form-control inputInn" name="buildermail">
+                                        </div>
+                                    </div>
+                                    <div class="staffFrm">
+                                        <div class="col-md-6 inputbox">
+                                            <label class="control-label mb-2 inputIn">Salary / Monthly:</label>
+                                            <input type="number" class="form-control inputInn" name="buildermail">
+                                        </div>
+                                        <div class="form-group col-md-6 inputbox">
+                                            <label class="control-label mb-2 inputIn">From Branch :</label>
+                                            <input type="text" class="form-control inputInn" name="buildermail">
+                                        </div>
+                                    </div>
+
+                                    <div class="staffFrm1">
+                                        <div class="col-md-6 inputbox">
+                                            <label class="control-label mb-2 inputIn">Joining Date:</label>
+                                            <input type="date" class="form-control inputInn" name="buildermail">
+                                        </div>
+                                        <div class="form-group col-md-3 ">
+                                            <button class="back">Back</button>
+                                        </div>
+                                        <div class="form-group col-md-3 ">
+                                            <button class="submit">Submit</button>
+                                        </div>
+                                    </div>
+
+
+                                </div>
+                            </div>
+                            <div>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
                         </div>
+
                         <!-- /.container-fluid -->
                     </body>
+
+
                 </div>
                 <!-- End of Main Content -->
 
@@ -124,7 +198,7 @@
                     <div class="container my-auto">
                         <div class="copyright text-center my-auto">
                             <div class="footer-widget__copyright">
-                                <p class="mini_text" style="color:black"> ©2024 Ask-Oncologist . All Rights Reserved. Designed &
+                                <p class="mini_text" style="color:black"> ©2024 ----- . All Rights Reserved. Designed &
                                     Developed by <a href="https://bhavicreations.com/" target="_blank" style="text-decoration: none;color:black">Bhavi
                                         Creations</a></p>
                             </div>
